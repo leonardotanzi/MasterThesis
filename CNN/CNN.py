86d42791d9702cc4106ff76778d742a02937beaf
leonardotanzi <leo.tanzi@gmail.com>
2019-04-25 17:24:16 +0200

    logs and plot

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -3,8 +3,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.utils import plot_model
 
-from sklearn.model_selection import train_test_split
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -13,13 +14,12 @@ from tqdm import tqdm
 import random
 import time
 
-DATADIR = "/Users/leonardotanzi/Desktop/Dataset"
+DATADIR = "/Users/leonardotanzi/Desktop/MasterThesis/CNN"
 
 CATEGORIES = ["Broken", "Unbroken"]
 
-NAME = "FirstNN-{}".format(int(time.time()))
-
-tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+# NAME = "FirstNN-{}".format(int(time.time()))
+# tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 
 for category in CATEGORIES:
     path = os.path.join(DATADIR, category)  # create path to broken and unbroken
@@ -68,31 +68,49 @@ X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1) # we need to convert x in num
 
 X = X/255.0
 
-model = Sequential()
+dense_layers = [0]
+layer_sizes = [64]
+conv_layers = [3]
 
-model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:])) #64 is the number of filter used
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+            NAME = "{}conv-{}nodes-{}dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+            print(NAME)
+            model = Sequential()
 
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:])) #64 is the number of filter used
+            model.add(Activation("relu"))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dense(64))
-model.add(Activation("relu"))
+            for l in range(conv_layer - 1):  # because we need for sure 1 conv layer
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation("relu"))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+                # model.add(Dropout(0.25))
 
-model.add(Dense(1))
-model.add(Activation("sigmoid"))
+            model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
-model.compile(loss="binary_crossentropy",
-              optimizer="adam",
-              metrics=["accuracy"])
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation("relu"))
+                # model.add(Dropout(0.5))
 
-model.fit(X, y, batch_size=32, epochs=15, validation_split=0.3, callbacks=[tensorboard])
+            model.add(Dense(1))
+            model.add(Activation("sigmoid"))
 
-model.save("firstModel.model")
+            model.compile(loss="binary_crossentropy",
+                          optimizer="adam",
+                          metrics=["accuracy"])
+
+            model.fit(X, y, batch_size=32, epochs=15, validation_split=0.3, callbacks=[tensorboard])
+
+            model.summary()
+            plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+            model.save("firstModel.model")
 
 
 
