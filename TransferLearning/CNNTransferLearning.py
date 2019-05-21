8f3eb8454473d0111fd4030fc827642cb909d8d2
leonardotanzi <leo.tanzi@gmail.com>
2019-05-21 14:49:48 +0200

    Server connection

--- TransferLearning/CNNTransferLearning.py
+++ TransferLearning/CNNTransferLearning.py
@@ -1,19 +1,19 @@
-from tensorflow.python.keras.applications import ResNet50, inception_v3
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D
-from tensorflow.python.keras.applications.resnet50 import preprocess_input
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.utils import plot_model
+from tensorflow.keras.applications import ResNet50, inception_v3
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import plot_model
 
 
 
 num_classes = 3
 image_size = 256
-resnet_weights_path = "/Users/leonardotanzi/Desktop/MasterThesis/TransferLearning/" \
+resnet_weights_path = "/home/ltanzi//MasterThesis/TransferLearning/" \
                       "resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5"
 
 my_new_model = Sequential()
-my_new_model.add(ResNet50(include_top=False, pooling='avg', weights=resnet_weights_path))
+my_new_model.add(ResNet50(include_top=False, pooling='max', weights=resnet_weights_path))
 my_new_model.add(Dense(num_classes, activation='softmax'))
 
 # Say not to train first layer (ResNet) model. It is already trained
@@ -22,22 +22,24 @@ my_new_model.layers[0].trainable = False
 my_new_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-#FIT model
+# Fit model
 data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
 
-train_generator = data_generator.flow_from_directory("/Users/leonardotanzi/Desktop/FinalDataset/Train_Val/Train",
+# Takes the path to a directory & generates batches of augmented data.
+train_generator = data_generator.flow_from_directory("/mnt/Data/ltanzi/Train_Val/Train",
         target_size=(image_size, image_size),
         batch_size=24,
         class_mode='categorical')
 
-validation_generator = data_generator.flow_from_directory("/Users/leonardotanzi/Desktop/FinalDataset/Train_Val/Validation",
+validation_generator = data_generator.flow_from_directory("/mnt/Data/ltanzi/Train_Val/Validation",
         target_size=(image_size, image_size),
         class_mode='categorical')
 
+# Trains the model on data generated batch-by-batch by a Python generator
+# When you use fit_generator, the number of samples processed for each epoch is batch_size * steps_per_epochs.
 my_new_model.fit_generator(
         train_generator,
-        steps_per_epoch=3,
-        epochs=10,
+        epochs=5,
         validation_data=validation_generator,
         validation_steps=1)
 
