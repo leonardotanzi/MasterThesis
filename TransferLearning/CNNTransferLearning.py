a6eb25c7cea3cbbef8ea94a244886f2859996fc6
leonardotanzi <leo.tanzi@gmail.com>
2019-06-12 15:58:51 +0200

    Mod Cam

--- TransferLearning/CNNTransferLearning.py
+++ TransferLearning/CNNTransferLearning.py
@@ -74,7 +74,7 @@ es = EarlyStopping(monitor="val_acc", mode = "max", verbose=1, patience=3) # ver
 
 my_new_model = Sequential()
 # my_new_model.add(ResNet50(include_top=False, pooling="avg", weights='imagenet'))
-my_new_model.add(VGG16(include_top=False, pooling="avg", weights="imagenet"))
+my_new_model.add(VGG16(include_top=False, input_shape=(224, 224, 3), pooling="avg", weights="imagenet"))
 
 my_new_model.layers[0].summary()
 # my_new_model.add(Dense(32, activation="relu"))
