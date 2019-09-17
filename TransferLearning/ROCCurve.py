ee66e15254ba6861c1a1b37d831b73258b230ebb
leonardotanzi <leo.tanzi@gmail.com>
2019-09-17 16:24:07 +0200

    roc

--- TransferLearning/ROCCurve.py
+++ TransferLearning/ROCCurve.py
@@ -4,35 +4,21 @@ import os
 from tqdm import tqdm
 import random
 import numpy as np
-from sklearn.metrics import roc_curve, auc
+from scipy import interp
 import matplotlib.pyplot as plt
-from scipy.interpolate import UnivariateSpline
+from itertools import cycle
+from sklearn.metrics import roc_curve, auc
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import glob
+import argparse
 
-
-def plot_pdf(y_pred, y_test, name=None, smooth=500):
-    positives = y_pred[y_test == 1]
-    negatives = y_pred[y_test == 0]
-    N = positives.shape[0]
-    n = N//smooth
-    s = positives
-    p, x = np.histogram(s, bins=n) # bin it into n = N//10 bins
-    x = x[:-1] + (x[1] - x[0])/2   # convert bin edges to centers
-    f = UnivariateSpline(x, p, s=n)
-    plt.plot(x, f(x))
-
-    N = negatives.shape[0]
-    n = N//smooth
-    s = negatives
-    p, x = np.histogram(s, bins=n) # bin it into n = N//10 bins
-    x = x[:-1] + (x[1] - x[0])/2   # convert bin edges to centers
-    f = UnivariateSpline(x, p, s=n)
-    plt.plot(x, f(x))
-    plt.xlim([0.0, 1.0])
-    plt.xlabel('density')
-    plt.ylabel('density')
-    plt.title('PDF-{}'.format(name))
-    plt.show()
-
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server", required=True, help="Running the code on the server or not (y/n)")
+ap.add_argument("-m", "--model", required=True, help="Model used: 0 VGG, 1 ResNet, 2 Inception")
+args = vars(ap.parse_args())
+run_on_server = args["server"]
 
 def print_img(name, img):
     cv2.namedWindow(name)
@@ -42,11 +28,34 @@ def print_img(name, img):
     cv2.destroyWindow(name)
 
 
-datadir = "/Users/leonardotanzi/Desktop/Test" #"/mnt/Data/ltanzi/Train_Val/Test"
-categories = ["A", "B"]
+if run_on_server == 'y':
+    datadir = "/mnt/data/ltanzi/Train_Val_CV/Test"
+    model = "/mnt/data/ltanzi/CV/Fold1_lr00001-batch32-notAugValTest-retrainAll-balanced-categorical-baselineInception-1563972372.model"
+elif run_on server == 'n':
+    datadir = "/Users/leonardotanzi/Desktop/Test"
+    model = "/Users/leonardotanzi/Desktop/Fold1_lr00001-batch32-notAugValTest-retrainAll-balanced-categorical-baselineInception-1563972372.model"
+
+categories = ["A", "B", "Unbroken"]
 img_size = 299
 training_data = []
+n_classes = 3
+y_score = []
 
+data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+model = tf.keras.models.load_model(model)
+
+for img_path in sorted(glob.glob(datadir + "/*.png"), key=os.path.getsize):
+    img = image.load_img(img_path, target_size=(img_size, img_size))
+
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    y_score.append(model.predict(x))
+    # y_score = model.predict(X)
+
+'''
 for category in categories:
 
     path = os.path.join(datadir, category)  # create path to broken and unbroken
@@ -69,43 +78,111 @@ for features, label in training_data:
     X.append(features)
     y.append(label)
 
-X = np.array(X).reshape(-1, img_size, img_size, 1) # we need to convert x in numpy array, last 1 because it's grayscale
+# X = np.array(X).reshape(-1, img_size, img_size, 1) # we need to convert x in numpy array, last 1 because it's grayscale
 
-X = X/255.0
+# X = X/255.0
+
+for x in X:
+    x = preprocess_input(x)
 
 model = tf.keras.models.load_model("/Users/leonardotanzi/Desktop/Cascade/Fold4_IncV3-A_B-categorical-baselineInception-1568304568-best_model.h5")
 
-'''
 score = model.evaluate(X, y, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 '''
-y_pred = model.predict(X).ravel()
-fpr, tpr, thresholds = roc_curve(y, y_pred)
 
-auc_AB = auc(fpr, tpr)
+# Plot linewidth.
+lw = 2
 
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+# Compute macro-average ROC curve and ROC area
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
 plt.figure(1)
-plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr, tpr, label='Keras (area = {:.3f})'.format(auc_AB))
-# plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-plt.title('ROC curve')
-plt.legend(loc='best')
-plt.show()
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right")
+
+if run_on_server == 'n':
+    plt.show()
+else:
+    plt.savefig("/mnt/data/ltanzi/MasterThesis/TransferLearning/Roc1.png")
+
+
 # Zoom in view of the upper left corner.
 plt.figure(2)
 plt.xlim(0, 0.2)
 plt.ylim(0.8, 1)
-plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr, tpr, label='Keras (area = {:.3f})'.format(auc_AB))
-# plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-plt.title('ROC curve (zoomed in at top left)')
-plt.legend(loc='best')
-plt.show()
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
 
-# plot_pdf(y_pred_rf, y_test, 'rf')
-plot_pdf(y_pred, y, 'Keras')
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right")
+
+if run_on_server == 'n':
+    plt.show()
+else:
+    plt.savefig("/mnt/data/ltanzi/MasterThesis/TransferLearning/Roc2.png")
