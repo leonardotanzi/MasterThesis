0bc9cce907e15ee68c6e2f3e8cab9270c2094a08
leonardo_tanzi <leo.tanzi@gmail.com>
2019-09-26 10:51:05 +0200

    s

--- TransferLearning/A1A2A3_CNN.py
+++ TransferLearning/A1A2A3_CNN.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
                         last_layer = 2
                         classmode = "sparse"
                         act = "softmax"
-                        classes = ["A", "B"]
+                        classes = ["A1", "A2"]
                         name = "Fold{}_{}_{}-{}-baseline{}-{}".format(i, classes[0], classes[1], binary, model_type, int(time.time()))
 
                 elif run_binary == "n":
